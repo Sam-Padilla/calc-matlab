f7d26bdef47ac0465aeb5304d08a6ae1c45c58ca
Sam <sscambara@gmail.com>
2019-03-11 10:45:52 -0600

    view and controller buttons was connected

--- src/controller/AppController.java
+++ src/controller/AppController.java
@@ -6,11 +6,179 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class AppController {
-       
-     @FXML 
-     public void button8Click(ActionEvent event) throws IOException{
+        
+    @FXML public void buttonPPlusClick(ActionEvent event) throws IOException{
         Button button = (Button) event.getSource();
         System.out.println(button.getText());
     }
     
+    @FXML public void buttonPMinusClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonPMultipClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonPDivisionClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    //----
+    
+    @FXML public void buttonEClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonESquaredClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonSE2xClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonSE3xClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    //----
+    
+    @FXML public void buttonIClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonIDefClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonDClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonDDefClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    //----
+    
+    @FXML public void buttonUndefinedClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    //@FXML public void buttonUndefinedClick(ActionEvent event) throws IOException{
+    //    Button button = (Button) event.getSource();
+    //    System.out.println(button.getText());
+    //}
+    
+    //@FXML public void buttonUndefinedClick(ActionEvent event) throws IOException{
+    //    Button button = (Button) event.getSource();
+    //    System.out.println(button.getText());
+    //}
+    
+    @FXML public void buttonCClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    //----
+   
+    @FXML public void button7Click(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void button4Click(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void button1Click(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonEqualsClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    
+    //----
+    
+    @FXML public void button8Click(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void button5Click(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void button2Click(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonPointClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+
+    //----
+    
+    @FXML public void button9Click(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void button6Click(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void button3Click(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void button0Click(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+     //----
+    
+    @FXML public void buttonPlusClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonMinusClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonMultiClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
+    
+    @FXML public void buttonDivisionClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
 }
