e789bb22ca4e7bd996c4759ee1a60a2debd142e7
Sam Padilla <sscambara@gmail.com>
2019-03-11 09:21:58 -0600

    starting with the functionality on the buttons

--- src/controller/AppController.java
+++ src/controller/AppController.java
@@ -1,14 +1,16 @@
 package controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
-public class AppController implements Initializable {
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
+public class AppController {
+       
+     @FXML 
+     public void button8Click(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
+    }
     
 }
